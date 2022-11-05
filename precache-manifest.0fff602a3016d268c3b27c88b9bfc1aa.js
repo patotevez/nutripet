2c02ca115ef7084afd6467172de315f161587d8a
Patricio Tevez <patz.oldschool@gmail.com>
2022-11-05 15:33:15 -0300

    Updates

---
+++ precache-manifest.0fff602a3016d268c3b27c88b9bfc1aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b39f1450e67c1384913",
+    "url": "/nutripet/static/css/main.aa3b18c9.chunk.css"
+  },
+  {
+    "revision": "7b39f1450e67c1384913",
+    "url": "/nutripet/static/js/main.7b39f145.chunk.js"
+  },
+  {
+    "revision": "bc20d6d6d939309784cb",
+    "url": "/nutripet/static/js/1.bc20d6d6.chunk.js"
+  },
+  {
+    "revision": "a017352067647fb6d3ef",
+    "url": "/nutripet/static/js/2.a0173520.chunk.js"
+  },
+  {
+    "revision": "b671931e131889751f70",
+    "url": "/nutripet/static/js/runtime~main.b671931e.js"
+  },
+  {
+    "revision": "89f75e554e98c6cd8e00a6587726e9d6",
+    "url": "/nutripet/index.html"
+  }
+];
